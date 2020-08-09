216ef6d2486f8b6f8c066f3d605f4b8864fd7b29
Daniel Prilik <danielprilik@gmail.com>
2020-08-09 14:01:01 -0400

    flatten pp/timers
    i've been meaning to do this for quite a while lol

--- clicky-core/src/devices/platform/pp.rs
+++ clicky-core/src/devices/platform/pp.rs
@@ -1,6 +1,7 @@
 //! Platform support for the PortalPlayer 50xx line of SoCs.
 
 mod cachecon;
+mod cfg_timer;
 mod cpucon;
 mod cpuid;
 mod devcon;
@@ -16,9 +17,10 @@ mod memcon;
 mod piezo;
 mod ppcon;
 mod serial;
-mod timers;
+mod usec_timer;
 
 pub use cachecon::*;
+pub use cfg_timer::*;
 pub use cpucon::*;
 pub use cpuid::*;
 pub use devcon::*;
@@ -34,7 +36,7 @@ pub use memcon::*;
 pub use piezo::*;
 pub use ppcon::*;
 pub use serial::*;
-pub use timers::*;
+pub use usec_timer::*;
 
 pub mod common {
     #[derive(Debug, Copy, Clone, PartialEq, Eq)]
