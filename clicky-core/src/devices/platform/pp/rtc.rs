0ce49c690c49a7f7e5a231a63ef251c591b193d0
Jean THOMAS <virgule@jeanthomas.me>
2026-08-11 22:52:23 +0200

    rtc: Use relativity

--- clicky-core/src/devices/platform/pp/rtc.rs
+++ clicky-core/src/devices/platform/pp/rtc.rs
@@ -1,14 +1,16 @@
 use crate::devices::prelude::*;
-use chrono::{Datelike, Local, Timelike};
+use relativity::Instant;
 
 #[derive(Debug)]
 pub struct Rtc {
-
+    reset_time: Instant
 }
 
 impl Rtc {
 	pub fn new() -> Rtc {
-		Rtc {}
+		Rtc {
+            reset_time: Instant::now()
+        }
 	}
 }
 
@@ -31,20 +33,21 @@ impl Memory for Rtc {
     fn r32(&mut self, offset: u32) -> MemResult<u32> {
         match offset {
             0x00 => {
-                let now = Local::now();
-                let packed = (now.second() & 0x3f)
-                    | ((now.minute() & 0x3f) << 8)
-                    | ((now.hour() & 0x3f) << 16)
-                    | ((now.day() & 0x3f) << 24);
-                Ok(packed)
-            }
+                let delta = Instant::now() - self.reset_time;
+                Ok(delta.as_millis() as u32)
+            },
             _ => Err(Unexpected),
         }
     }
 
     fn w32(&mut self, offset: u32, _val: u32) -> MemResult<()> {
         match offset {
-            0x00 => Ok(()), // Written by RetailOS during boot
+            // Written by RetailOS & diagnostics during boot
+            // Read in "5 in 1" diagnostic menu, but not decoded
+            0x00 => {
+                self.reset_time = Instant::now();
+                Ok(())
+            },
             _ => Err(Unexpected)
         }
     }
