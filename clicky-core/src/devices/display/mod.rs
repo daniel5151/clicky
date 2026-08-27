db58be6a81c8440dab52c58335132bc1e6c677c5
Jean THOMAS <virgule@jeanthomas.me>
2026-08-27 12:08:56 +0200

    display: Add LcdPanel trait
    Add LcdPanel trait that models the communication channel between PP LCD
    controller and the LCD panel driver IC.

--- clicky-core/src/devices/display/mod.rs
+++ clicky-core/src/devices/display/mod.rs
@@ -1,3 +1,27 @@
 //! Display-related devices.
 
+use crate::devices::prelude::*;
+
+use crate::gui::RenderCallback;
+
 pub mod hd66753;
+
+/// LCD Controller IC trait (eg. HD66753)
+pub trait LcdPanel: std::fmt::Debug + Send + Sync {
+    /// Select a register / issue a command (i.e: write the Index Register).
+    fn write_command(&mut self, val: u16) -> MemResult<()>;
+
+    /// Read back the command register.
+    fn read_command(&mut self) -> MemResult<u16>;
+
+    /// Write to the currently selected register.
+    fn write_data(&mut self, val: u16) -> MemResult<()>;
+
+    /// Read from the currently selected register.
+    fn read_data(&mut self) -> MemResult<u16>;
+
+    /// Returns a callback which renders the panel's framebuffer.
+    ///
+    /// The callback accepts a framebuffer, and returns the rendered dimensions.
+    fn render_callback(&self) -> RenderCallback;
+}
